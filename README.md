b7dfe5a2adb2e49006aace6b492a34e981e41285
Daniel Dumke <ddumke@gmx.net>
2014-06-22 21:15:01 +0200

    First draft of the classes

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Portfolio
 =========
 
-Portfolio wealth management app for the command line
+Portfolio wealth management app for the command line.
+
+Todos
+=====
+* write classes
+* Add sqlite for stock quotes and rest
+* ...
+
--- portfolio.py
+++ portfolio.py
@@ -1,1 +1,37 @@
-#
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Analyses:
+    """Perform additional analyses on the Portfolios and stocks."""
+    pass
+    
+class Portfolio:
+    """Collection of different portfolios or stocks."""
+    pass
+
+class Security:
+    """Class for stocks, bond, cash, everything is a security."""
+    def __init__(self, name):
+        self.name = name
+        self.id = id
+        # number of stocks/nominale
+        self.prices = Prices(self.id)
+class Transaction:
+    """Class to store transactions"""
+    def __init__(self):
+        self.id 
+    
+    
+
+class Prices:
+    """Class to store price developments."""
+    pass
+    
+class UI:
+    """Class to display user interface."""
+    pass
+    
+if __name__ == "__main__":
+    UI = UI()
+    

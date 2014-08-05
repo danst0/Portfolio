f9b425615c616ce192db5daf4f2ccda2dba4af63
Daniel Dumke <ddumke@gmx.net>
2014-08-05 21:52:49 +0200

    Modularization

---
+++ Portfolio.py
@@ -0,0 +1,25 @@
+class Portfolio:
+	"""Collection of different portfolios or stocks."""
+	def __init__(self, name, parent=None):
+		self.parent = parent
+		self.name = name
+		self.children = []
+		self.child_securities = []
+		self.cash = 0.0
+	def add_security(self, parent, stock_id):
+		if self.name == parent:
+			self.child_securities.append(stock_id)
+		else:
+			for i in self.children:
+				i.add_security(parent, stock_id)
+	def add_portfolio(self, parent, name):
+		if self.name == parent:
+			self.children.append(Portfolio(name, parent))
+		else:
+			for i in self.children:
+				i.add_portfolio(parent, name)
+	def __str__(self, level=1):
+		output = self.name + '\n'
+		for i in self.children:
+			output += '	   '*level + '+-- ' + i.__str__(level+1)
+		return output
--- pf.py
+++ pf.py
@@ -16,7 +16,7 @@ import uuid
 import subprocess
 import string
 from Database import Database
-
+from Portfolio import Portfolio
 
 import matplotlib.pyplot as plt
 # Sample stocks		   
@@ -44,31 +44,7 @@ class Analyses:
 	
 
 	 
-class Portfolio:
-	"""Collection of different portfolios or stocks."""
-	def __init__(self, name, parent=None):
-		self.parent = parent
-		self.name = name
-		self.children = []
-		self.child_securities = []
-		self.cash = 0.0
-	def add_security(self, parent, stock_id):
-		if self.name == parent:
-			self.child_securities.append(stock_id)
-		else:
-			for i in self.children:
-				i.add_security(parent, stock_id)
-	def add_portfolio(self, parent, name):
-		if self.name == parent:
-			self.children.append(Portfolio(name, parent))
-		else:
-			for i in self.children:
-				i.add_portfolio(parent, name)
-	def __str__(self, level=1):
-		output = self.name + '\n'
-		for i in self.children:
-			output += '	   '*level + '+-- ' + i.__str__(level+1)
-		return output
+
 		
 class Security:
 	"""Class for stocks, bond, cash, everything is a security."""

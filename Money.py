56386196df21b1ee95dc8c26db6d670b5817c455
Daniel <daniel@dumke.me>
2014-08-09 12:51:11 +0200

    new Money class with stumps

---
+++ Money.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Money:
+    def __init__(self):
+        pass
+    
+    def add_income(self, date, income):
+        pass
+            
+    def add_total(self, date, total):
+        pass
+    
+    def get_all(self, 
+        income, expense, portfolio, total = 0,0,0,0
+        return income, expense, portfolio, total

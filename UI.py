1fac7fd75d7f7acb2323641cf94c8afd29cce133
Daniel Dumke <ddumke@gmx.net>
2014-08-18 20:43:43 +0200

    Rolling profitabiliy over last 12 month

--- UI.py
+++ UI.py
@@ -215,6 +215,56 @@ class UI:
 		if do_delete.lower() == 'yes':
 			self.prices.delete_prices(stock_obj.isin_id)
 			self.secs.delete_stock(stock_obj.isin_id)			  
+	
+	def rolling_profitability(self):
+		portfolio = input('Portfolio [All] ')
+		if portfolio == '':
+			portfolio = 'All'
+		time_span = 360
+		tmp_default = (datetime.date.today() - datetime.timedelta(days=time_span)).strftime('%Y-%m-%d')
+		from_date = input('Start date [' + tmp_default + '] ')
+		if from_date == '':
+			from_date = tmp_default
+		from_date = datetime.datetime.strptime(from_date, "%Y-%m-%d").date()
+		tmp_default = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+		to_date = input('End date [' + tmp_default + '] ')
+		if to_date == '':
+			to_date = tmp_default
+		to_date = datetime.datetime.strptime(to_date, "%Y-%m-%d").date()
+		dates = []
+		roi_list = []
+		for i in range(int(time_span/30)):
+			loop_to_date = (datetime.date.today() - datetime.timedelta(days=1+i*30))			
+			loop_from_date = (datetime.date.today() - datetime.timedelta(days=time_span+i*30))
+			stocks_at_start = self.transaction.get_portfolio('All', loop_from_date.strftime("%Y-%m-%d"))
+			portfolio_value_at_start = 0.0
+			for key in stocks_at_start.keys():
+				portfolio_value_at_start += stocks_at_start[key] * self.prices.get_last_price_from_stock_id(key, loop_from_date.strftime("%Y-%m-%d"), none_equals_zero=True)
+			stocks_at_end = self.transaction.get_portfolio('All', loop_to_date.strftime("%Y-%m-%d"))
+			portfolio_value_at_end = 0.0
+			for key in stocks_at_end.keys():
+				portfolio_value_at_end += stocks_at_end[key] * self.prices.get_last_price_from_stock_id(key, loop_to_date.strftime("%Y-%m-%d"), none_equals_zero=True)
+
+			invest = self.transaction.get_total_invest('All', loop_from_date, loop_to_date)
+			divest = self.transaction.get_total_divest('All', loop_from_date, loop_to_date)
+			dividend = self.transaction.get_total_dividend('All', loop_from_date, loop_to_date)
+
+			profit_incl_on_books = portfolio_value_at_end + invest - divest - portfolio_value_at_start + dividend
+			if portfolio_value_at_start - invest != 0:
+				tmp = 100 * profit_incl_on_books/(portfolio_value_at_start - invest)
+			else:
+				tmp = 0.0
+			dates.append(loop_to_date)
+			roi_list.append(tmp)
+# 			print('Date', loop_to_date, 'ROI', tmp)
+		plt.plot(dates, roi_list, 'r')
+#		  plt.axis(dates)
+		plt.title('Rolling Return-on-Investment; range: ' + from_date.strftime('%Y-%m-%d') + ' -- ' + to_date.strftime('%Y-%m-%d'))
+		plt.xlabel('%')
+		plt.xticks(rotation=15)
+		plt.xlabel('Date')
+		plt.show()
+
 	def profitability(self):
 		portfolio = input('Portfolio [All] ')
 		if portfolio == '':
@@ -351,10 +401,12 @@ class UI:
 		return self.new_menu(
 			[	'List portfolio',
 				'Saving development',
-				'Profitability'],
+				'Profitability',
+				'Rolling profitability'],
 			[	self.list_portfolio,
 				self.savings,
-				self.profitability], inp)
+				self.profitability,
+				self.rolling_profitability], inp)
 	def settings_menu(self, inp=''):
 		return self.new_menu(
 			[	'Planned saving (p.m.)'],
@@ -448,7 +500,7 @@ class UI:
 
 				os.remove(base_path + '/' + file)
 		os.remove(base_path + '/data.txt')
-# 		sys.exit()
+#		sys.exit()
 		for file in os.listdir(base_path):
 			if (file.startswith('HV-BEGLEIT') or
 				file.startswith('KONTOABSCH') or

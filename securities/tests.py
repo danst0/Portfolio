ff5be011ff84c8d345e60e2ddb83c5879e115772
Daniel Dumke <ddumke@gmx.net>
2014-08-29 20:06:08 +0200

    make test work for import quotes

--- securities/tests.py
+++ securities/tests.py
@@ -1,10 +1,8 @@
 
 from django.test import TestCase
 import datetime
-from decimal import *
-from transactions.models import Portfolio, Transaction
 from securities.models import Security, Price
-from django.utils import timezone
+from decimal import *
 
 # Create your tests here.
 
@@ -17,12 +15,14 @@ class PriceTests(TestCase):
         pass
 
     def test_import_historic_quotes(self):
-        """Adding Transaction should lead to correct total"""
+        """
+        Adding Transaction should lead to correct total
+        Test only works as long as we scrape enough years (or date is recent enough)!!!
+        """
         sec = Security()
         sec.add('Test', '', '', 'APC.DE', 'Stock')
         p = Price()
         p.import_historic_quotes(years=0.5)
-        date = timezone.now() - datetime.timedelta(days=30)
+        date = datetime.datetime.strptime('01.08.2014', '%d.%m.%Y')
         price = p.get_last_price_from_stock_id(stock_id=sec.find('Test'), before_date=date)
-        print(price)
-        self.assertEqual(price, None)
+        self.assertEqual(price.price, Decimal('70.93'))

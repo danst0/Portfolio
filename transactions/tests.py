d8d373bd54f409aac8a0798eb0ffbb7564411ca8
Daniel Dumke <ddumke@gmx.net>
2014-08-30 16:09:11 +0200

    test for portfolio overview

--- transactions/tests.py
+++ transactions/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 import datetime
 from decimal import *
 from transactions.models import Portfolio, Transaction
-from securities.models import Security
+from securities.models import Security, Price
 from django.utils import timezone
 
 # Create your tests here.
@@ -34,3 +34,24 @@ class TransactionTests(TestCase):
         t = Transaction()
         self.assertRaises(NameError, t.add, 'b', pf, sec, time, 100, 100, 100)
 
+    def test_portfolio_overview(self):
+        sec = Security.objects.create(name='TestSec', aliases='Test Alias', isin_id='DETest', yahoo_id='ABC',
+                                      type='Stock')
+        pf = Portfolio.objects.create(name='Test')
+        t = Transaction()
+        time = timezone.now()
+        price = Price.objects.create(stock_id=sec, date=time, price=Decimal(111))
+        t.add('b', pf, sec, time, 100, 100, 100)
+
+        p = t.list_pf(pf.name, time)
+        self.assertEqual(p, [{
+                                 'nominal': Decimal('100'),
+                                 'invest': Decimal('-10100'),
+                                 'profit': Decimal('1000'),
+                                 'name': 'TestSec',
+                                 'value': Decimal('11100'),
+                                 'price': Decimal('111'),
+                                 'cost': Decimal('100')},
+                             {
+                                 'name': 'Total',
+                                 'value': Decimal('11100')}])
